d1a3b50a0b7a5c20af6780398fa8f7f9f626fb3f
Matúš Lizoň <matus.lizon@edgecom.eu>
2021-01-16 17:32:07 +0100

    galery fin

--- src/JavaScripts/gallery.js
+++ src/JavaScripts/gallery.js
@@ -24,6 +24,7 @@ if(galleryImages){
             let newImg= document.createElement("img");
            newImgWindow.appendChild(newImg);
             newImg.setAttribute("src", "images/"+setNewImgUrl);
+           newImg.setAttribute("id", "current-img");
 
                 newImg.onload= function(){
 
@@ -33,11 +34,11 @@ if(galleryImages){
 
                let newNextBtn =document.createElement("a");
                let btnNextText = document.createTextNode("Next") ;
-               newPrevBtn.appendChild(btnPrevText);
-               container.appendChild(newPrevBtn);
+               newNextBtn.appendChild(btnNextText);
+               container.appendChild(newNextBtn);
                newNextBtn.setAttribute("class","img-btn-next");
-               newNextBtn.setAttribute("onclick","changeImg()");
-               newNextBtn.style.cssText = ""
+               newNextBtn.setAttribute("onclick","changeImg(1)");
+               newNextBtn.style.cssText = "right : "+ calcImgToEdge +"px";
 
 
                let newPrevBtn =document.createElement("a");
@@ -45,7 +46,8 @@ if(galleryImages){
                newPrevBtn.appendChild(btnPrevText);
                container.appendChild(newPrevBtn);
                newPrevBtn.setAttribute("class","img-btn-prev");
-               newPrevBtn.setAttribute("onclick","changeImg()");
+               newPrevBtn.setAttribute("onclick","changeImg(0)");
+                    newPrevBtn.style.cssText = "left : "+ calcImgToEdge +"px";
            }
 
 
@@ -56,5 +58,47 @@ if(galleryImages){
 }
 function closeImg() {
     document.querySelector(".img-window").remove();
+    document.querySelector(".img-btn-next").remove();
+    document.querySelector(".img-btn-prev").remove();
+}
+
+function changeImg(changeDir) {
+    document.querySelector("#current-img").remove();
+
+    let getImgWindow = document.querySelector(".img-window");
+    let newImg = document.createElement("img");
+    getImgWindow.appendChild(newImg);
+
+    let calcNewImg;
+    if(changeDir===1)
+    {
+    calcNewImg=getLatestOpenedImg + 1;
+    if(calcNewImg > 6){
+        calcNewImg =1;
+            }
+    }
+    else if(changeDir===0){
+        calcNewImg=getLatestOpenedImg-  1;
+        if(calcNewImg < 1 ){
+            calcNewImg =6;
+        }
+    }
+
+    newImg.setAttribute("src", "images/img" +calcNewImg+ ".jpg");
+    newImg.setAttribute("id","current-img");
+    getLatestOpenedImg =calcNewImg;
+
+    newImg.onload = function () {
+        let imgWidth=this.width;
+        let calcImgToEdge = ((windowWidth - imgWidth)/2) - 80;
+
+        let nextBtn = document.querySelector(".img-btn-next");
+        nextBtn.style.cssText = "right : "+ calcImgToEdge +"px";
+
+        let prevBtn = document.querySelector(".img-btn-prev");
+        prevBtn.style.cssText = "left : "+ calcImgToEdge +"px";
+    }
+
+
 
 }
